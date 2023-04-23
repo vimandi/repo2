f9bce7333bd892122bfcc154a3087fa6028028e9
Admin <Admin@DESKTOP-PRKNFF1>
2023-04-23 10:19:54 +0530

    added comment as first commit

---
+++ demo3/src/main/java/com/example/demo3/Demo3Application.java
@@ -0,0 +1,14 @@
+package com.example.demo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo3Application.class, args);
+		System.out.println("add demo3 comment");
+	}
+
+}
