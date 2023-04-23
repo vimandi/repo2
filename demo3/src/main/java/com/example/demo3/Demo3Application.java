9361b8a8ee5d1707f7b43f394a0dddc417645789
Admin <Admin@DESKTOP-PRKNFF1>
2023-04-23 11:10:59 +0530

    revert log changes

--- demo3/src/main/java/com/example/demo3/Demo3Application.java
+++ demo3/src/main/java/com/example/demo3/Demo3Application.java
@@ -8,7 +8,6 @@ public class Demo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo3Application.class, args);
-		System.out.println("add demo3 comment");
 	}
 
 }
